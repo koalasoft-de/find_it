4fb9395088897478334390d07f6a3092f1ac8dc5
Kevin Bulteel <kb@koalasoft.de>
2023-10-23 20:32:21 +0200

    go version

---
+++ go/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	files, _ := ioutil.ReadDir("./")
+
+	os.Remove("./leaked.txt")
+
+	pwFile, _ := os.ReadFile("./passwords.txt")
+	passwords := strings.Split(string(pwFile), "\n")
+	channels := []chan int{}
+	leaked, _ := os.OpenFile("./leaked.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+
+	defer leaked.Close()
+
+	for _, file := range files {
+
+		if !strings.HasPrefix(file.Name(), "log.") {
+			continue
+		}
+
+		c := make(chan int)
+
+		channels = append(channels, c)
+
+		go scanLog(c, file.Name(), leaked, passwords)
+	}
+
+	lines := 0
+
+	for _, c := range channels {
+		lines += <-c
+	}
+
+	fmt.Printf("Scanned %v lines", lines)
+}
+
+func scanLog(c chan int, file string, out *os.File, passwords []string) {
+	log, _ := os.Open(file)
+	lines := 0
+
+	defer log.Close()
+
+	scanner := bufio.NewScanner(log)
+
+	for scanner.Scan() {
+		lines++
+
+		for _, pw := range passwords {
+			line := scanner.Text()
+
+			if line == "" || pw == "" {
+				continue
+			}
+
+			if strings.Contains(line, pw) {
+				i1 := strings.Index(line, ": ") + 2
+				i2 := strings.Index(line, "failed") - 1
+
+				res := fmt.Sprintf("Leak found: %v | Password: %v | Log File: %v\n", line[i1:i2], pw, file)
+
+				fmt.Print(res)
+				out.WriteString(res)
+			}
+		}
+	}
+
+	c <- lines
+}
